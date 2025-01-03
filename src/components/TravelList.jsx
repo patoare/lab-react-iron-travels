2389386b333faf7ae3f0280825a6ae94ce5b9585
patricio <patoare@hotmail.com>
2025-01-03 12:00:15 +0100

    done

--- src/components/TravelList.jsx
+++ src/components/TravelList.jsx
@@ -1,37 +1,39 @@
 import React from 'react';
 import travelPlansData from "../assets/travel-plans.json";
+import {useState} from 'react';
 
 const TravelList = () => {
-    return(
-        <div>
-            <ul>
-                {travelPlansData.map((plan) => (
-                  <li key={plan.id}>
-                  {plan.destination}
-                  <img src={plan.image} />
-                  {plan.days}
-                  {plan.allInclusive ? 'yes' : "no"}
-                  ${plan.totalCost}
-                  {plan.description}
-                  <ul>
-                  {plan.parts.map((part, index) =>(
-                    <li key={index}>
-                        {part.name}
-                        {part.description}
-                        {part.cost}
-                    </li>
-                  ))}
-                  </ul>
-                  </li>
-    ))}
-    </ul>
-          </div>
-      )
+  const [travelPlans, setTravelPlans] = useState(travelPlansData)
+
+  const handleDelete = (id) => {
+ setTravelPlans(travelPlans.filter(currentPlan => currentPlan.id != id))
   }
-
-              
-
-
-
+    return(
+      <ul>
+        {travelPlans.map(currentPlan => (
+          <li key={currentPlan.id} style= {{display: 'flex', border: '1px solid black', height: '300px',width:'1000px', padding: '1rem', paddingBottom: '1rem', gap:'10rem'}}>
+            <div>
+              <img src= {currentPlan.image} style={{width: '300px'}} />
+            </div>
+            <div>
+              <h2>
+                 {currentPlan.destination} ({currentPlan.days} Days)
+              </h2>
+              <p>
+                 {currentPlan.description}
+              </p>
+              <p>
+                 Price: {currentPlan.totalCost} $
+              </p>
+              {currentPlan.totalCost < 350 && <p style={{fontWeight: 'bolder', color:'blue', border: '1px solid blue'}}>Great Deal</p>}
+              {currentPlan.totalCost > 1500 && <p style={{fontWeight: 'bolder', color:'red', border: '1px solid red'}}>Premium</p>}
+              {currentPlan.allInclusive && <p style={{fontWeight: 'bolder', color:'green', border: '1px solid green'}}>All Inclusive</p>}
+              <button type='button' onClick={() => handleDelete(currentPlan.id)}>Delete</button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    )
+  }
 
 export default TravelList
